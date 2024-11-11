8aa23523bd88b0c3ee21ddcadcf91ff289a79e6f
Eraldo Jaku <eraldo.jaku@outlook.com>
2024-11-11 17:43:53 +0100

    Add unit test

--- internal/repository/repoistory_test.go
+++ internal/repository/repoistory_test.go
@@ -80,4 +80,47 @@ func TestCRUD(t *testing.T) {
 	tags, err := tagRepo.ListTags(ctx)
 	require.NoError(t, err)
 	require.Empty(t, tags)
+
+	// Add 1 tag
+	tag, err := tagRepo.CreateTag(ctx, "Some tag")
+	require.NoError(t, err)
+	require.Equal(t, "Some tag", tag.Name)
+
+	//Create media with tag
+	media, err := mediaRepo.CreateMedia(ctx, "Some fancy picture", []string{tag.ID.String()}, "test")
+	require.NoError(t, err)
+	require.Equal(t, "Some fancy picture", media.Name)
+
+	// List tags should contain 1
+	tags, err = tagRepo.ListTags(ctx)
+	require.NoError(t, err)
+	require.Len(t, tags, 1)
+
+	// Add a second tag
+	tag2, err := tagRepo.CreateTag(ctx, "Some other tag")
+	require.NoError(t, err)
+
+	// Create a second media with both tags
+	media2, err := mediaRepo.CreateMedia(ctx, "Some other fancy picture", []string{tag2.ID.String(), tag.ID.String()}, "test")
+	require.NoError(t, err)
+
+	// List based on tag 1 should be 2 medias
+	medias, err = mediaRepo.ListMediaByTagId(ctx, tag.ID.String())
+	require.NoError(t, err)
+	require.Len(t, medias, 2)
+
+	// List based on tag 2 should be only the second media
+	medias, err = mediaRepo.ListMediaByTagId(ctx, tag2.ID.String())
+	require.NoError(t, err)
+	require.Len(t, medias, 1)
+	require.Equal(t, media2.Name, medias[0].Name)
+
+	// Get the tags provided in the array
+	tags, err = tagRepo.GetTags(ctx, []string{tag.ID.String()})
+	require.NoError(t, err)
+	require.Equal(t, tag.Name, tags[0].Name)
+
+	// 1 tag exist the other does not, should throw an error
+	_, err = tagRepo.GetTags(ctx, []string{tag.ID.String(), uuid.NewString()})
+	require.Error(t, err)
 }
